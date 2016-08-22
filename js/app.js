37b1c3ecba3430683799acbc668998fb878ceb50
Dkaf <Daniel.j.kafer@gmail.com>
2016-08-22 15:17:49 -0500

    added styling
    added some simple styling

--- js/app.js
+++ js/app.js
@@ -4,17 +4,31 @@ $(function(){
     var searchTerm = $('#query').val();
     getRequest(searchTerm);
   });
-})
+});
 
 function getRequest(searchTerm){
   var params = {
-    part: '',
-    key: 'AIzaSyD5mBcHDlTxWAURf-Oab6LuRhJpIVTVqrI',
-    q: searchTerm
+    part: 'snippet',
+    q: searchTerm,
+    key: 'AIzaSyD5mBcHDlTxWAURf-Oab6LuRhJpIVTVqrI'
   };
-  url = "https://www.googleapis.com/youtube/v3/search"
+  url = 'https://www.googleapis.com/youtube/v3/search';
 
   $.getJSON(url, params, function(data){
-    showResults(data.Search);
+    showResults(data.items);
+  });
+}
+
+$('.button').on('click', function(){
+  $('#search-results').empty();
+})
+
+function showResults(results) {
+  $.each(results, function(index, value){
+      var thumbnail = $('<img>', {src: value.snippet.thumbnails.medium.url, class:'thumbnail'});
+      var vidTitle = $('<span>' + value.snippet.title + '</span>')
+      $('#search-results').append(thumbnail);
+      $('#search-results').append(vidTitle).addClass('title');
+      console.log(value.snippet.title);
   });
 }
